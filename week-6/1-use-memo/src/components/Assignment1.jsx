bc08738d342ad42f7139acca0ec62cf512c75ebd
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-01-12 21:53:59 +0530

    Solves Week-6 1-use-memo

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useState,useMemo } from "react";
 
-// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
+// In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input.
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
-    const [input, setInput] = useState(0);
-    // Your solution starts here
-    const expensiveValue = 0; 
-    // Your solution ends here
+  const [input, setInput] = useState(0);
+  const [btn, setBtn] = useState(false);
+  // Your solution starts here
+  let expensiveValue = 0;
+  console.log("rendered again");
+  function factorial(n) {
+    if (n === 0) {
+      console.log("calculating...");
+      return 1;
+    } else return n * factorial(n - 1);
+  }
 
-    return (
-        <div>
-            <input 
-                type="number" 
-                value={input} 
-                onChange={(e) => setInput(Number(e.target.value))} 
-            />
-            <p>Calculated Value: {expensiveValue}</p>
-        </div>
-    );
+//   withUseMemo
+  expensiveValue = useMemo(() => {
+    return factorial(input);
+  }, [input]);
+
+//   without useMemo - gets called everytime a rerender happens
+//   expensiveValue = factorial(input);
+
+  return (
+    <div>
+      <input
+        type="number"
+        value={input}
+        onChange={(e) => setInput(Number(e.target.value))}
+      />
+      <button onClick={() => setBtn(!btn)}>render page</button>
+      <p>Calculated Value: {expensiveValue}</p>
+    </div>
+  );
 }
