1e1d4114d2c44ca239d2bd3c00027e9d2f997e4b
Arnav Singhal <arnavsinghal@Arnavs-MacBook-Pro.local>
2024-01-29 21:54:23 +0530

    Changes background color of signin page

--- week-8/paytm/frontend/src/pages/Signin.jsx
+++ week-8/paytm/frontend/src/pages/Signin.jsx
@@ -5,24 +5,24 @@ import Button from "../components/Button";
 import BottomWarning from "../components/BottomWarning";
 
 function Signin() {
-    return ( 
-        <div>
-            <div className="bg-slate-300 h-screen flex justify-center items-center">
-      <div className="bg-white rounded-lg flex flex-col items-center px-6 py-9 border">
-        <Heading label={"Sign In"} />
-        <SubHeading label={"Enter your credentials to access your account"} />
-        <InputBox label={"Email"} placeholder={"johndoe@gmail.com"} />
-        <InputBox label={"Password"} placeholder={"********"} />
-        <Button label={"Sign In"} />
-        <BottomWarning
-          label={"Don't have an account?"}
-          buttonText={"Sign up!"}
-          to={"/signin"}
-        />
+  return (
+    <div>
+      <div className="bg-[#7F7F7F] h-screen flex justify-center items-center">
+        <div className="bg-white rounded-lg flex flex-col items-center px-6 py-9 border">
+          <Heading label={"Sign In"} />
+          <SubHeading label={"Enter your credentials to access your account"} />
+          <InputBox label={"Email"} placeholder={"johndoe@gmail.com"} />
+          <InputBox label={"Password"} placeholder={"********"} />
+          <Button label={"Sign In"} />
+          <BottomWarning
+            label={"Don't have an account?"}
+            buttonText={"Sign up!"}
+            to={"/signin"}
+          />
+        </div>
       </div>
     </div>
-        </div>
-     );
+  );
 }
 
 export default Signin;
